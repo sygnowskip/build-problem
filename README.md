f9bc527611989302ef504e555dc1685c7ca95a66
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-06-07 14:27:02 +0200

    Example project with build problem

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # build-problem
 Show the build problem between VS 2019 / VS 2017 / Rider / .NET CLI
+
+Build status:
+* VS 2019 - Success
+* VS 2017 - Fail
+* Rider 2019.x - Fail
+* .NET CLI - Fail
+
+Why?! :)
+
+ERROR:
+
+Build FAILED.
+
+Validation.cs(21,18): error CS0121: The call is ambiguous between the following methods or properties: 'AsyncResultExtensionsRightOperand.Ensure(Result, Func<Task<bool>>, string)' and 'ResultExtensions.Ensure(Result, Func<bool>, string)' [.\build-problem\Test.csproj]
+    0 Warning(s)
+    1 Error(s)
+
+Time Elapsed 00:00:00.91
---
+++ Validation.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+
+namespace Test
+{
+    public class ValidationResult
+    {
+        public ValidationResult(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+    }
+    public class Validation
+    {
+        private static readonly Regex EmailAddressPattern = new Regex(@"example-pattern");
+        public static Result<ValidationResult> Validate(string emailAddress)
+        {
+            return Result.Ok()
+                .Ensure(EmailAddressIsValid, "Invalid address email")
+                .OnSuccess(() => new ValidationResult(true));
+
+
+            bool EmailAddressIsValid() => EmailAddressPattern.IsMatch(emailAddress);
+        }
+    }
+}
